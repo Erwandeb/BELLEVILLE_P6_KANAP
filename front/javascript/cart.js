2812d8592bc94cbdc3069ac05c143296cfbd6646
Erwan de Belleville <edebelleville@hotmail.fr>
2022-07-22 22:01:08 +0200

    modifs

--- front/javascript/cart.js
+++ front/javascript/cart.js
@@ -4,7 +4,7 @@ const displaySelecteditem = document.getElementById("cart__items");
 let colorChoice = "no-color";
 let quantityChoice;
 let quantityAddInCart;
-const id ="";
+const id = "";
 let canapes = [];
 
 //const id = localStorage.getItem('id');
@@ -16,53 +16,38 @@ let canapes = [];
 
 //getProductFromLocalStorage();
 
-/*
-for(let i=0; i<localStorage.length; i++) {
-    let key = localStorage.key( i );
-    console.log(key)
 
-    //item.push(JSON.parse(localStorage.getItem( key )));
-    //console.log("test", item);
-}
-*/
-
-
+// Initialising 
+const canapeInCart = getProducts();
 
 canapes.push(JSON.parse(localStorage.getItem( "basket" )));
 
-console.log(canapes[0])
+console.log("zzoz",canapes[0])
 /**
  * Affichage des éléments dynamiques dans le DOM
  * @classe voir scipt "classes.js"
  * 
  */
-
-
-// Faire forEach()
-
-
-
-/*
-    for(canape of canapes[0]){
+    canapeInCart.forEach(canape => {
         fetch(`http://localhost:3000/api/products/${canape.idProduit}`)
         .then((response) => {
             return response.json()
         })
         .then(data => {
+
+            
+
+         
             const produit = new Produit(data.colors, data._id, data.name, data.price, data.imageUrl, data.description, data.altText);
-            displaySelecteditem.innerHTML = produit.renderItemDetailsOnCart(canape.quantite, canape.couleur);
-        })
-        .catch(error => console.log("error dans le fetch"+ error)) 
-    }
-    */
-    canapes[0].forEach(canape => {
-        fetch(`http://localhost:3000/api/products/${canape.idProduit}`)
-        .then((response) => {
-            return response.json()
-        })
-        .then(data => {
-            const produit = new Produit(data.colors, data._id, data.name, data.price, data.imageUrl, data.description, data.altText);
-            displaySelecteditem.innerHTML = produit.renderItemDetailsOnCart(canape.quantite, canape.couleur);
+           
+            console.log(produit);
+            
+            displaySelecteditem.innerHTML += produit.renderItemDetailsOnCart(canape.quantite, canape.couleur);
+
+            document.querySelectorAll('div.deleteItem').addEventListener('click', ()=>{
+                console.log("testing delete")
+            });
+            
         })
         .catch(error => console.log("error dans le fetch"+ error)) 
     });
@@ -71,20 +56,13 @@ console.log(canapes[0])
 
 
 
-
-    /*
-    fetch(`http://localhost:3000/api/products/${element.idProduit}`)
-    .then((response) => {
-        return response.json()
-    })
-    .then(data => {
-        const produit = new Produit(data.colors, data._id, data.name, data.price, data.imageUrl, data.description, data.altText);
-        displaySelecteditem.innerHTML = produit.renderItemDetailsOnCart(quantity, color);
-    })
-    .catch(error => console.log("error dans le fetch"+ error)) 
-    */ 
-
-
-
-
 //loadItemInCart();
+
+// Fonction de supression des mots clés
+function deleteKeyWord(list, element){
+    const index = list.indexOf(element)
+    if(index > -1 || index === -1){ 
+        list.splice(index, 1);
+        return list;
+    }
+}
--- front/javascript/storage.js
+++ front/javascript/storage.js
@@ -1,28 +1,15 @@
 const addProduct  = (produit) => {
     const products = getProducts();
-    products.push(produit);
 
-
-    // Traitment des doublons
-    console.log('products', products)
-
-    let arraySansDoublon = deleteDoublon(products)
-
-    console.log("test2" , arraySansDoublon)
-    
-    let arraySansDoublon2 = hasDuplicates(products)
-    console.log("test2" , arraySansDoublon2);
-
-    const found = products.find(element => element.idProduit > 10);
-    console.log(found)
-    
-    const test22 = products.filter(onlyUnique);
-
-    console.log("ya", test22);
-    // Fin traitement doublons
-
-
-
+    const product = products.find(element => element.idProduit === produit.idProduit &&  element.couleur === produit.couleur );
+    if(!product){
+        products.push(produit);
+    }else{
+        product.quantite += produit.quantite;
+        if(product.quantite > 100 ){
+            product.quantite = 100;
+        }
+    }
 
     saveBasket(products);
 }
@@ -30,50 +17,3 @@ const addProduct  = (produit) => {
 const basketKey = 'basket';
 const getProducts = () => JSON.parse(localStorage.getItem(basketKey) ?? "[]")
 const saveBasket = (products) => localStorage.setItem(basketKey, JSON.stringify(products))
-
-
-// Find pour éviter les doublons 
-function remove_duplicates(arr) {
-    var obj = {};
-    var ret_arr = [];
-    for (var i = 0; i < arr.length; i++) {
-        obj[arr[i]] = true;
-    }
-    for (var key in obj) {
-        ret_arr.push(key);
-    }
-    return ret_arr;
-}
-
-function deleteDoublon(tableauOrigine){
-    let sansDoublon = new Set(tableauOrigine);
-    return Array.from(sansDoublon);
-}
-
-function hasDuplicates(array) {
-    return (new Set(array)).size !== array.length;
-}
-
-
-function onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
-  }
-  
-
-
-/*
-  const getProductFromLocalStorage = () =>{
-
-    console.log(localStorage.basket)
-    const item = [];
-
-    for(let i=0; i<localStorage.basket.length; i++) {
-        let key = localStorage.key( i );
-        console.log(key, i)
-    
-        item.push(JSON.parse(localStorage.getItem( "basket" )));
-        console.log("test", item);
-    }
-  }
-
-  */
